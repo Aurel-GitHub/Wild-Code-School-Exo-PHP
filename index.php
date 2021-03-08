a1547c2e858d42d25ab499fd6d7875ab5ea330cc
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-03-08 14:38:56 +0100

    exo7

--- index.php
+++ index.php
@@ -1,6 +1,40 @@
 <?php
 
-function writeSecretSentence($param1, $param2){
-    return $param1. ' s\'incline face à '.$param2; 
-} 
- echo writeSecretSentence('pas d idee ',' qqch');
+$characters = [
+
+    "Negan" => [
+
+        "city" => "The Sanctuary",
+
+        "weapon" => "Lucille"
+
+    ],
+
+    "Daryl" => [
+
+        "city" => "Alexandria",
+
+        "weapon" => "crossbow"
+
+    ],
+
+    "Ezekiel" => [
+
+        "city" => "The Kingdom",
+
+        "weapon" => "Shiva"
+
+    ],
+    "Aurélien" => [
+        "city" => "Coruscant",
+        "weapon" => "light saber"
+    ]
+
+];
+
+var_dump($characters);
+
+/**
+ * LIEN SCREENSHOT
+ * http://www.noelshack.com/2021-10-1-1615210510-capture-d-ecran-du-2021-03-08-14-32-59.png
+ */
