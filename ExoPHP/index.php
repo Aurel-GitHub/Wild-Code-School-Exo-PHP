3deb6ed5c2daa31e708c29fe6c48e95d132cb278
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-03-08 10:20:58 +0100

    exo 1

---
+++ ExoPHP/index.php
@@ -0,0 +1,8 @@
+
+<?php
+$title = 'Indiana Jones and the Last Crusade';
+$hasBeenWatched = true;
+$years = 1989;
+$note = 8.2;
+
+echo 'le film : '.$title. ' a été déjà vu : '.$hasBeenWatched. ' le : ' .$years. ' moyenne : '.$note;  
