51223011218341eeea823938272beeaab8d64744
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-03-08 14:11:48 +0100

    exo6

--- index.php
+++ index.php
@@ -1,24 +1,6 @@
 <?php
 
-
-$weapons = ['fists', 'whip', 'gun'];
-$ennemyWeapon = $weapons[rand(0,2)];
-
-echo "Arme de l'opposant: ".$ennemyWeapon. '  || ';
-
-if ($ennemyWeapon == 'fists')
-{
-    $indyWeapon = 'gun';
-    echo "Arme d'Indiana Jones: ".$indyWeapon
-    ;
-}
-elseif ($ennemyWeapon == "whip")
-{
-    $indyWeapon = 'fists';
-    echo "Arme d'Indiana Jones: ".$indyWeapon;
-}
-else
-{
-    $indyWeapon = 'whip';
-    echo "Arme d'Indiana Jones: ".$indyWeapon;
-}
+function writeSecretSentence($param1, $param2){
+    return $param1. ' s\'incline face à '.$param2; 
+} 
+ echo writeSecretSentence('pas d idee ',' qqch');
