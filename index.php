a6ddcc0d877a183b5ad3b89f3c92d587881e45ee
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-03-09 14:18:18 +0100

    delete comments

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
 
 ///// EXO I /////
-// function repeat_str($number, $word){
-//     echo str_repeat($word, $number);
-// }
-// repeat_str(5, 'Salut ');
+function repeat_str($number, $word){
+    echo str_repeat($word, $number);
+}
+repeat_str(5, 'Salut ');
 
 ///////////////////////////////////
 
 ////// EXO II //////
-function greet($name){
+
+function geetII($name){
     if($name == 'Johnny'){
-        return "Hello, my love!";
+        echo 'Hello, my love!';
     }else{
-        return "Hello, $name!";
+        echo 'Hello '.$name;
     }
 }
 
-greet('Jim');
+geetII('jean');
 
 
 
