7c8e38e693495cceca346c64f65d56e552269267
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2021-03-09 14:11:33 +0100

    exo codewars

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+///// EXO I /////
+// function repeat_str($number, $word){
+//     echo str_repeat($word, $number);
+// }
+// repeat_str(5, 'Salut ');
+
+///////////////////////////////////
+
+////// EXO II //////
+function greet($name){
+    if($name == 'Johnny'){
+        return "Hello, my love!";
+    }else{
+        return "Hello, $name!";
+    }
+}
+
+greet('Jim');
+
+
+
